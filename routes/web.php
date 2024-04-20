6ed12eabbef4ddc78e501de53298c2b2c4ac4146
takumaharuta <apple.0201tk@gmail.com>
2024-04-20 14:58:09 +0900

    LTカレッジの初コミットメント

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+Route::get('/posts', [PostController::class, 'index']);   
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
