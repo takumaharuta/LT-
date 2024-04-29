47ecb60f790c4e60fb487b69968530d7c8fe68b3
takumaharuta <apple.0201tk@gmail.com>
2024-04-29 20:34:29 +0900

    頑張ったやつ

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-Route::get('/posts', [PostController::class, 'index']);   
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index'] );
+Route::get('/posts/{post}', [PostController::class ,'show']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
